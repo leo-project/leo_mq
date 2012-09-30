e3af73f4f8128e7199ca7332ca8aeca111cac60c
yosukehara <yousuke.hara@gmail.com>
2012-09-30 23:07:58 +0900

    Remove an unnecessary definition in test

--- test/leo_mq_api_tests.erl
+++ test/leo_mq_api_tests.erl
@@ -26,7 +26,6 @@
 %%====================================================================
 -module(leo_mq_api_tests).
 -author('yosuke hara').
--vsn('0.9.1').
 
 -include("leo_mq.hrl").
 -include_lib("eunit/include/eunit.hrl").
