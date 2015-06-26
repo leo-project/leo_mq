83a6525290ff63374373a9a7fb31e575e227b973
Yosuke Hara <yousuke.hara@gmail.com>
2015-06-26 16:10:56 +0900

    Refactor mq-consumer

--- src/leo_mq_consumer.erl
+++ src/leo_mq_consumer.erl
@@ -237,20 +237,18 @@ idling(#event_info{event = ?EVENT_RUN}, From, #state{id = Id,
                                                      batch_of_msgs = BatchOfMsgs,
                                                      interval = Interval} = State) ->
     NextStatus = ?ST_RUNNING,
-    State_1 = State#state{status = NextStatus,
+    State_1 = State#state{status = ?ST_IDLING,
                           start_datetime = leo_date:now()},
     gen_fsm:reply(From, ok),
     ok = run(Id),
     ok = leo_mq_publisher:update_consumer_stats(PublisherId, NextStatus, BatchOfMsgs, Interval),
     {next_state, NextStatus, State_1, ?DEF_TIMEOUT};
-idling(#event_info{event = ?EVENT_STATE}, From, State) ->
-    NextStatus = ?ST_IDLING,
-    gen_fsm:reply(From, {ok, NextStatus}),
-    {next_state, NextStatus, State, ?DEF_TIMEOUT};
+idling(#event_info{event = ?EVENT_STATE}, From, #state{status = Status} = State) ->
+    gen_fsm:reply(From, {ok, Status}),
+    {next_state, ?ST_IDLING, State#state{status = Status}, ?DEF_TIMEOUT};
 idling(_, From, State) ->
     gen_fsm:reply(From, {error, badstate}),
-    NextStatus = ?ST_IDLING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT}.
+    {next_state, ?ST_IDLING, State#state{status = ?ST_IDLING}, ?DEF_TIMEOUT}.
 
 -spec(idling(EventInfo, State) ->
              {next_state, ?ST_IDLING, State} when EventInfo::#event_info{},
@@ -262,7 +260,7 @@ idling(#event_info{event = ?EVENT_RUN}, #state{id = Id,
     NextStatus = ?ST_RUNNING,
     ok = run(Id),
     ok = leo_mq_publisher:update_consumer_stats(PublisherId, NextStatus, BatchOfMsgs, Interval),
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT};
+    {next_state, NextStatus, State#state{status = ?ST_IDLING}, ?DEF_TIMEOUT};
 
 idling(#event_info{event = ?EVENT_INCR},
        #state{mq_properties = #mq_properties{regular_batch_of_msgs = BatchOfMsgs,
@@ -280,13 +278,11 @@ idling(#event_info{event = ?EVENT_DECR},
     {ok, {StepBatchOfMsgs, StepInterval}} = ?step_comsumption_values(MQProps),
     BatchOfMsgs_1 = decr_batch_procs_fun(BatchOfMsgs, StepBatchOfMsgs),
     Interval_1 = incr_interval_fun(Interval, MaxInterval, StepInterval),
-    NextStatus = ?ST_IDLING,
-    {next_state, NextStatus, State#state{status = NextStatus,
+    {next_state, ?ST_IDLING, State#state{status = ?ST_IDLING,
                                          batch_of_msgs = BatchOfMsgs_1,
                                          interval = Interval_1}, ?DEF_TIMEOUT};
 idling(_, State) ->
-    NextStatus = ?ST_IDLING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT}.
+    {next_state, ?ST_IDLING, State#state{status = ?ST_IDLING}, ?DEF_TIMEOUT}.
 
 
 %% @doc State of 'running'
@@ -405,19 +401,16 @@ running(#event_info{event = ?EVENT_DECR},
                                          status = NextStatus}, ?DEF_TIMEOUT};
 
 running(_, State) ->
-    NextStatus = ?ST_RUNNING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT}.
+    {next_state, ?ST_RUNNING, State#state{status = ?ST_RUNNING}, ?DEF_TIMEOUT}.
 
 -spec(running( _, _, #state{}) ->
              {next_state, ?ST_RUNNING, #state{}}).
-running(#event_info{event = ?EVENT_STATE}, From, State) ->
-    NextStatus = ?ST_RUNNING,
-    gen_fsm:reply(From, {ok, NextStatus}),
-    {next_state, NextStatus, State, ?DEF_TIMEOUT};
+running(#event_info{event = ?EVENT_STATE}, From, #state{status = Status} = State) ->
+    gen_fsm:reply(From, {ok, Status}),
+    {next_state, ?ST_RUNNING, State#state{status = ?ST_RUNNING}, ?DEF_TIMEOUT};
 running(_, From, State) ->
-    NextStatus = ?ST_RUNNING,
     gen_fsm:reply(From, {error, badstate}),
-    {next_state, NextStatus, State, ?DEF_TIMEOUT}.
+    {next_state, ?ST_RUNNING, State#state{status = ?ST_RUNNING}, ?DEF_TIMEOUT}.
 
 
 %% @doc State of 'suspend'
@@ -426,12 +419,10 @@ running(_, From, State) ->
              {next_state, ?ST_SUSPENDING, State} when EventInfo::#event_info{},
                                                       State::#state{}).
 suspending(#event_info{event = ?EVENT_RUN}, State) ->
-    NextStatus = ?ST_SUSPENDING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT};
+    {next_state, ?ST_SUSPENDING, State#state{status = ?ST_SUSPENDING}, ?DEF_TIMEOUT};
 
 suspending(#event_info{event = ?EVENT_STATE}, State) ->
-    NextStatus = ?ST_SUSPENDING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT};
+    {next_state, ?ST_SUSPENDING, State#state{status = ?ST_SUSPENDING}, ?DEF_TIMEOUT};
 
 suspending(#event_info{event = ?EVENT_INCR},
            #state{id = Id,
@@ -459,8 +450,7 @@ suspending(#event_info{event = ?EVENT_INCR},
                                          batch_of_msgs = BatchOfMsgs_1,
                                          interval = Interval_1}, ?DEF_TIMEOUT};
 suspending(_, State) ->
-    NextStatus = ?ST_SUSPENDING,
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT}.
+    {next_state, ?ST_SUSPENDING, State#state{status = ?ST_SUSPENDING}, ?DEF_TIMEOUT}.
 
 -spec(suspending(EventInfo, From, State) ->
              {next_state, ?ST_SUSPENDING | ?ST_RUNNING, State} when EventInfo::#event_info{},
@@ -471,18 +461,15 @@ suspending(#event_info{event = ?EVENT_RESUME}, From, #state{id = Id,
                                                             batch_of_msgs = BatchOfMsgs,
                                                             interval = Interval} = State) ->
     gen_fsm:reply(From, ok),
-    NextStatus = ?ST_RUNNING,
     ok = run(Id),
-    ok = leo_mq_publisher:update_consumer_stats(PublisherId, NextStatus, BatchOfMsgs, Interval),
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT};
-suspending(#event_info{event = ?EVENT_STATE}, From, State) ->
-    NextStatus = ?ST_SUSPENDING,
-    gen_fsm:reply(From, {ok, NextStatus}),
-    {next_state, NextStatus, State#state{status = NextStatus}, ?DEF_TIMEOUT};
+    ok = leo_mq_publisher:update_consumer_stats(PublisherId, ?ST_RUNNING, BatchOfMsgs, Interval),
+    {next_state, ?ST_RUNNING, State#state{status = ?ST_RUNNING}, ?DEF_TIMEOUT};
+suspending(#event_info{event = ?EVENT_STATE}, From, #state{status = Status} = State) ->
+    gen_fsm:reply(From, {ok, Status}),
+    {next_state, ?ST_SUSPENDING, State#state{status = ?ST_SUSPENDING}, ?DEF_TIMEOUT};
 suspending(_Other, From, State) ->
-    NextStatus = ?ST_SUSPENDING,
     gen_fsm:reply(From, {error, badstate}),
-    {next_state, NextStatus, State, ?DEF_TIMEOUT}.
+    {next_state, ?ST_SUSPENDING, State#state{status = ?ST_SUSPENDING}, ?DEF_TIMEOUT}.
 
 
 %%--------------------------------------------------------------------
